6870fa6e9b328e11ce8bf8f72e411ee5fd72b0b1
yeeun0702 <kimyeeun0702@naver.com>
2025-03-04 16:19:58 +0900

    ✨ [feat] Lv1. 양의 정수 및 연산기호 입력받기

--- src/main/java/org/mandatory/level1/App.java
+++ src/main/java/org/mandatory/level1/App.java
@@ -1,7 +1,24 @@
 package org.mandatory.level1;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = scanner.nextInt();
+
+        int result;
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = scanner.next().charAt(0);
+
+        scanner.close();
 
     }
+
 }

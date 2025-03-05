aca608bbe0d4efc381dd7615ff3d1cb55a169b63
yeeun0702 <kimyeeun0702@naver.com>
2025-03-05 11:06:07 +0900

    ✨ [feat] #1 예외처리 구현

---
+++ src/main/java/org/mandatory/refactored/exception/DivideByZeroException.java
@@ -0,0 +1,7 @@
+package org.mandatory.refactored.exception;
+
+public class DivideByZeroException extends ArithmeticException {
+    public DivideByZeroException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/mandatory/refactored/exception/InvalidInputException.java
@@ -0,0 +1,7 @@
+package org.mandatory.refactored.exception;
+
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}

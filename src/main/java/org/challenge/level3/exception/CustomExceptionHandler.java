42386af1499a42e1d11c73a0e38bbb697f53bfb1
yeeun0702 <kimyeeun0702@naver.com>
2025-03-06 12:09:30 +0900

    ✨ [feat] #3 예외처리 패키지 분리

---
+++ src/main/java/org/challenge/level3/exception/CustomExceptionHandler.java
@@ -0,0 +1,7 @@
+package org.challenge.level3.exception;
+
+public class CustomExceptionHandler {
+    public static void handleException(Exception e) {
+        System.out.println(e.getMessage());
+    }
+}

585e9f046ee20194f4f75bf3c5ea88fd4feb9a1a
yeeun0702 <kimyeeun0702@naver.com>
2025-03-07 15:55:27 +0900

    ♻️ [refactor] #1 InputView에서 입력 검증 수행

--- src/main/java/org/mandatory/refactored/exception/InvalidInputException.java
+++ src/main/java/org/mandatory/refactored/exception/InvalidInputException.java
@@ -1,7 +1,5 @@
 package org.mandatory.refactored.exception;
 
 public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
+    public InvalidInputException(String message) {super(message);}
 }

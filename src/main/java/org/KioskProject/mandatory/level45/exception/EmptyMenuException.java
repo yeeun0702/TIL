8781054a9cab1bef7d26dc7356dd8f25dbef6c9c
yeeun0702 <kimyeeun0702@naver.com>
2025-03-14 04:16:30 +0900

    ✨ [feat] #5 CH2 키오스크 과제 Lv.4,5 예외 처리 추가

---
+++ src/main/java/org/KioskProject/mandatory/level45/exception/EmptyMenuException.java
@@ -0,0 +1,7 @@
+package org.KioskProject.mandatory.level45.exception;
+
+public class EmptyMenuException extends Exception {
+    public EmptyMenuException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/KioskProject/mandatory/level45/exception/InvalidInputException.java
@@ -0,0 +1,7 @@
+package org.KioskProject.mandatory.level45.exception;
+
+public class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}

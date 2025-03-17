8fcf7892b19d170457bd0729321ba2afab625a34
yeeun0702 <kimyeeun0702@naver.com>
2025-03-17 09:52:18 +0900

    ♻️ [refactor] #5 CH2 키오스크 과제 Lv.1 0 입력 시 프로그램 종료되는 기능을 if-else문에서 switch문으로 통합, 예외처리 추가

--- src/main/java/org/KioskProject/mandatory/level1/Main.java
+++ src/main/java/org/KioskProject/mandatory/level1/Main.java
@@ -1,30 +1,21 @@
 package org.KioskProject.mandatory.level1;
 
-import org.KioskProject.mandatory.level2.MenuItem;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        List<MenuItem> menuItems = new ArrayList<>();
-
         Scanner scanner = new Scanner(System.in);
 
         printMenu();
         while (true) {
             System.out.println("메뉴를 입력하세요. 0 입력 시 키오스크가 종료됩니다.");
-            int n = scanner.nextInt();
 
-            // 0 입력 시 프로그램 종료
-            if (n == 0) {
-                System.out.println("키오스크를 종료합니다.");
-                break;
-            }
+            int choice = scanner.nextInt();
 
-            switch(n){
+            switch(choice){
+                case 0:
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
                 case 1:
                     System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
                     break;
@@ -37,6 +28,8 @@ public class Main {
                 case 4:
                     System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
                     break;
+                default:
+                    throw new IllegalArgumentException("잘못된 입력입니다.");
             }
 
         }
